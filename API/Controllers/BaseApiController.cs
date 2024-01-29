71e07a5b643b1c19519edaa859ecae85ef2e70eb
onurozkinaci@gmail.com <onurozkinaci@gmail.com>
2024-01-30 01:04:10 +0300

    User entıty and db ıs updated.

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] //...../api/users(e.g. https://localhost:5001/api/users)
+
+public class BaseApiController:ControllerBase
+{
+
+}
