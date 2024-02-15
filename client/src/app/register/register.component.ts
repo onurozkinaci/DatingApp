5201009b1fe1c1120f817a69b5273ee58c25478a
onurozkinaci@gmail.com <onurozkinaci@gmail.com>
2024-02-15 23:40:07 +0300

    section5 is completed - hashing up the register method to API service.

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +10,19 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  @Input() usersFromHomeComponent:any;
+  //@Input() usersFromHomeComponent:any;
   @Output() cancelRegister = new EventEmitter(); //=>to emit something from child to parent.
-
+   constructor(private accountService:AccountService){
+   }
    model:any = {}
    register(){
-     console.log(this.model);
+     //=>the register operation will be triggered via our service;
+     this.accountService.register(this.model).subscribe({
+         next:() => {
+            this.cancel(); //=>to close the register form after the operation is completed.
+         },
+         error:error => console.log(error)
+     });
    }
    cancel(){
     this.cancelRegister.emit(false); //=>we want to turn off the registermode in home.component(parent component)
