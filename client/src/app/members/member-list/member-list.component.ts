446f6a76d4e0bbb6552668d1e3441210e4f5d217
onurozkinaci@gmail.com <onurozkinaci@gmail.com>
2024-03-31 01:49:58 +0300

    styling added for member cards

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Member } from '../../_models/member';
 import { MembersService } from '../../_services/members.service';
+import { MemberCardComponent } from "../member-card/member-card.component";
 
 @Component({
-  selector: 'app-member-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './member-list.component.html',
-  styleUrl: './member-list.component.css'
+    selector: 'app-member-list',
+    standalone: true,
+    templateUrl: './member-list.component.html',
+    styleUrl: './member-list.component.css',
+    imports: [MemberCardComponent]
 })
 export class MemberListComponent {
    members: Member[] = [];
